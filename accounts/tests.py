3ad39f9cbbecf1b29676f78c4b858917c1ba3658
Sina Arasteh <sinna.arasteh@gmail.com>
2025-04-04 18:24:15 +0330

    Test the 'index_page' function

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class IndexPageTests(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+    def test_render(self):
+        response = self.c.get("")
+        self.assertTemplateUsed(response, 'accounts/index.html')

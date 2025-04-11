53fa0afb2df85adfd5fcf3e5842ba7cd82891661
Sina Arasteh <sinna.arasteh@gmail.com>
2025-04-11 19:41:57 +0330

    Set the url for the login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from . import views
+from . import views, forms
+from django.contrib.auth import views as auth_views
 
 app_name = 'accounts'
 urlpatterns = [
     path('', views.index_page, name="index"),
     path('signup/', views.signup, name="signup"),
-    path('login/', views.login, name="login"),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name="accounts",
+            authentication_form=forms.CustomAuthenticationForm
+        ),
+        name="login"
+    ),
 ]

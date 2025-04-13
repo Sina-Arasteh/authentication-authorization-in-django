1cb8b6a646af4187d411eee2cda5fe9ebbb5373e
Sina Arasteh <sinna.arasteh@gmail.com>
2025-04-13 22:19:09 +0330

    Create the 'PremiumUser' model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PremiumUser(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True
+    )
+    is_premium = models.BooleanField('کاربر ویژه', default=False)
+
+    def __str__(self):
+        return self.user.username

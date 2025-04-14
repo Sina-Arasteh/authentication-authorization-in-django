688511796fa0c6a24129298fd6acacd27bcc97ee
Sina Arasteh <sinna.arasteh@gmail.com>
2025-04-14 21:16:19 +0330

    Add the 'is_premium' field to the 'User' page in the admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.apps import apps
+from .models import PremiumUser
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 
 models = apps.get_models()
@@ -8,4 +11,15 @@ for model in models:
         admin.site.register(model)
     except admin.sites.AlreadyRegistered:
         pass
-    
+
+admin.site.unregister(PremiumUser)
+
+class PremiumUserInline(admin.StackedInline):
+    model = PremiumUser
+    can_delete = False
+
+admin.site.unregister(User)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    inlines = [PremiumUserInline]
